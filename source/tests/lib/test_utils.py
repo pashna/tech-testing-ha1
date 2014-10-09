2f26ce4ec16c7f2af2d971567c13100dbc69b1f7
Pashna <p02p@yandex.ru>
2014-10-10 02:36:50 +0400

    bit refactor

--- source/tests/lib/test_utils.py
+++ source/tests/lib/test_utils.py
@@ -5,38 +5,49 @@ import mock
 from source.lib import utils
 
 class UtilsTestCase(unittest.TestCase):
+
     def test_my_first_test(self):
         self.assertFalse(1 == 2)
 
     def test_demonize__father(self):
         pid = 4242
-        with mock.patch('os.fork', mock.Mock(return_value=pid)) as os_fork:
-            with mock.patch('os._exit', mock.Mock()) as os_exit:
+        fork = mock.Mock(return_value=pid)
+        exit = mock.Mock()
+        with mock.patch('os.fork', fork):
+            with mock.patch('os._exit', exit):
                 utils.daemonize()
 
-        os_fork.assert_called_once_with()
-        os_exit.assert_called_once_with(0)
+        fork.assert_called_once_with()
+        exit.assert_called_once_with(0)
+
 
     def test_demonize__child_child(self):
         pid = 0
-        with mock.patch('os.fork', mock.Mock(return_value=pid)) as os_fork:
-            with mock.patch('os.setsid', mock.Mock()) as os_setsid:
-                with mock.patch('os._exit', mock.Mock()) as os_exit:
+        fork = mock.Mock(return_value=pid)
+        exit = mock.Mock()
+        setsid = mock.Mock()
+        with mock.patch('os.fork', fork):
+            with mock.patch('os.setsid', setsid):
+                with mock.patch('os._exit', exit):
                     utils.daemonize()
 
-        os_setsid.assert_called_once_with()
-        assert os_fork.called
-        assert not os_exit.called
+        setsid.assert_called_once_with()
+        assert fork.called
+        assert not exit.called
+
 
     def test_demonize__child_father(self):
         father_pid = 0
         child_pid = 4242
-        with mock.patch('os.fork', mock.Mock(side_effect=[child_pid, father_pid])) as os_fork:
+        fork = mock.Mock(side_effect=[child_pid, father_pid])
+        exit = mock.Mock()
+        with mock.patch('os.fork', fork):
             with mock.patch('os.setsid', mock.Mock()):
-                with mock.patch('os._exit', mock.Mock()) as os_exit:
+                with mock.patch('os._exit', exit):
                     utils.daemonize()
 
-        os_exit.assert_called_once_with(0)
+        exit.assert_called_once_with(0)
+
 
     def test_demonize__father_exception(self):
         oserror = OSError("err")
@@ -49,20 +60,20 @@ class UtilsTestCase(unittest.TestCase):
         pid = 0
         with mock.patch('os.fork', mock.Mock(side_effect=[pid,oserror])):
             with mock.patch('os.setsid', mock.Mock()): # as os_setsid:
-                """os_setsid.assert_called_once_with() !!!!!! why don't pass?"""
                 self.assertRaises(Exception, utils.daemonize)
 
+#===================================================================
 
     def test_create_pidfile(self):
         pid = 4242
         pidfile_path = "random_dir/random_file"
-        m_open = mock.mock_open()
-        with mock.patch('source.lib.utils.open', m_open, create=True):
+        open = mock.mock_open()
+        with mock.patch('source.lib.utils.open', open, create=True):
             with mock.patch('os.getpid', mock.Mock(return_value = pid)):
                 utils.create_pidfile(pidfile_path)
 
-        m_open.assert_called_once_with(pidfile_path, 'w')
-        m_open().write.assert_called_once_with(str(pid))
+        open.assert_called_once_with(pidfile_path, 'w')
+        open().write.assert_called_once_with(str(pid))
 
 
     def test_load_config_from_pyfile(self):
@@ -76,7 +87,7 @@ class UtilsTestCase(unittest.TestCase):
         })
         self.assertFalse(hasattr(config, "NameFour"))
         self.assertFalse(hasattr(config, "nameFive"))
-        self.assertFalse(hasattr(config, "name_six"))
+        self.assertFalse(hasattr(config, "name_six")) #BUG FOUNDED!!!
 
 
     def test_check_network_status__ok(self):
@@ -105,44 +116,11 @@ class UtilsTestCase(unittest.TestCase):
         result = utils.parse_cmd_args(args.split(' ')[1:], descr)
         self.assertEqual(result, argparse.Namespace(config=path, daemon=True, pidfile=str(pid)))
 
-    """def test_spawn_workers(self):
-        import multiprocessing
-        num = 5
-        utils.spawn_workers(num, mock.Mock(), mock.Mock(), mock.Mock())
-        with mock.patch('multiprocessing.Process', mock.Mock()) as process:
-            self.assertTrue(process.call_count, num)"""
-
 
     def test_spawn_workers(self):
         num = 10
+        process_mock = mock.Mock()
 
-        #WHY THIS PATH??????
-        #WHY THIS PATH??????
-        #WHY THIS PATH??????
-        #WHY THIS PATH??????
-        #WHY THIS PATH??????
-        #WHY THIS PATH??????       \/\/\/\/\/\/\/\/ i want multiprocessing.Process
-        with mock.patch("source.lib.utils.Process", mock.Mock()) as process_mock:
+        with mock.patch("source.lib.utils.Process", process_mock):
             utils.spawn_workers(num, mock.Mock(), mock.Mock(), mock.Mock())
-            #self.assertTrue(process_mock.called)
             self.assertEqual(process_mock.call_count, num)
-
-
-
-
-    #def test_get_tube(self):
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
